284385ab5b56bab575060822e4f41ae67ecee9d4
Red Hendery <red.hendery@gmail.com>
2016-03-15 06:27:27 +1300

    add logic enable / disable to Request Invitation feature

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,7 +1,10 @@
 import Ember from 'ember';
 
-export default Ember.Controller.extend({
+export default Ember.Controller.extend ({
 
-  isDisabled: true
+  emailAddress: '',
+
+  isValid: Ember.computed.match('emailAddress', /^.+@.+\..+$/),
+  isDisabled: Ember.computed.not('isValid')
 
 });
