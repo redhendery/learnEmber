99396002c98ca6db322203e8eec4fa38d1dfd1ca
Red Hendery <red.hendery@gmail.com>
2016-03-16 09:12:49 +1300

    add Model for contact form, form does not wowork

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -10,7 +10,6 @@ export default Ember.Controller.extend({
   actions: {
 
     saveInvitation() {
-      alert(`Saving of the following email address is in progress: ${this.get('emailAddress')}`);
       this.set('responseMessage', `Thank you! We have just saved your email address: ${this.get('emailAddress')}`);
       this.set('emailAddress', '');
     }
