349f4452c104ea24032d29ab3bb7b5175ed6dcc5
Red Hendery <red.hendery@gmail.com>
2016-03-16 08:14:35 +1300

    add contact form

--- app/controllers/contact.js
+++ app/controllers/contact.js
@@ -1,4 +1,18 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+
+  emailAddress: '',
+
+  isValid: Ember.computed.match('emailAddress', /^.+@.+\..+$/),
+  isDisabled: Ember.computed.not('isValid'),
+
+  actions: {
+
+    saveMessage() {
+      this.set('responseMessage', `Thank you! We will respond to you shortly! ${this.get('emailAddress')}`);
+      this.set('emailAddress', '');
+    }
+  }
+
 });
--- app/controllers/index.js
+++ app/controllers/index.js
@@ -11,7 +11,7 @@ export default Ember.Controller.extend({
 
     saveInvitation() {
       alert(`Saving of the following email address is in progress: ${this.get('emailAddress')}`);
-      this.set('responseMessage', `Thank you! We've just saved your email address: ${this.get('emailAddress')}`);
+      this.set('responseMessage', `Thank you! We have just saved your email address: ${this.get('emailAddress')}`);
       this.set('emailAddress', '');
     }
   }
