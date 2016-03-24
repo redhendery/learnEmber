d415b3242aa60b66b434338757b626677e163906
Red Hendery <red.hendery@gmail.com>
2016-03-24 18:54:40 +1300

    get email address save to firebase on invitation request

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -14,11 +14,16 @@ export default Ember.Controller.extend({
     saveInvitation() {
       const email = this.get('emailAddress');
 
-      const newInvitation = this.store.createRecord('invitation', { email: email });
-      newInvitation.save();
+      const newInvitation = this.store.createRecord('invitation', {
+        email: email
+      });
 
-      this.set('responseMessage', `Thank you! We have saved your email address: ${this.get('emailAddress')}`);
-      this.set('emailAddress', '');
+      newInvitation.save().then((response) => {
+        this.set('responseMessage', `Thank you! We have saved your email address with the following id: ${response.get('id')}`);
+        this.set('emailAddress', '');
+      });
+
     }
   }
+
 });
